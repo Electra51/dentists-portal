a83f63f77cdc540801906554c3875e2aadc38416
safayet nur <safayetnurelectra@gmail.com>
2025-11-18 22:59:25 +0600

    update: infoCards design update

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import About from './About';
-import Banner from './Banner';
-import Contact from './Contact';
-import InfoCards from './InfoCards';
-import MakeAppointment from './MakeAppointment';
-import Services from './Services';
-import Testimonial from './Testimonial';
+import React from "react";
+import About from "./About";
+import Banner from "./Banner";
+import Contact from "./Contact";
+import InfoCards from "./InfoCards";
+import MakeAppointment from "./MakeAppointment";
+import Services from "./Services";
+import Testimonial from "./Testimonial";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            <InfoCards></InfoCards>
-            <Services></Services>
-            <About></About>
-            <MakeAppointment></MakeAppointment>
-            <Testimonial></Testimonial>
-            <Contact></Contact>
-        </div>
-    );
+  return (
+    <div>
+      <Banner />
+      <InfoCards />
+      <Services />
+      <About />
+      <MakeAppointment />
+      <Testimonial />
+      <Contact />
+    </div>
+  );
 };
 
 export default Home;
